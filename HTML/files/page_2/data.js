dbf657ce6084152c8c1073382f2d946c86cfa099
hahachiu <fslyc528000@gmail.com>
2016-09-19 22:07:23 +0800

    project commit

---
+++ HTML/files/page_2/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m,n,o,p,q],r,_(s,t,u,v,w,x,y,_(),z,_(A,B,C,D,E,_(F,G,H,I),J,null,K,D,L,D,M,N,O,null,P,Q,R,S,T,U,V,Q),W,_(),X,_(),Y,_(Z,[])),ba,_(),bb,_());}; 
+var b="url",c="page_2.html",d="generationDate",e=new Date(1452252305685.76),f="isCanvasEnabled",g=false,h="variables",i="rollHeight",j="rollWidth",k="top",l="tempTop",m="rollHeight2",n="rollWidth2",o="top2",p="tempTop2",q="ifLoop",r="page",s="packageId",t="102aa9e713264ab79ec30252bea4646b",u="type",v="Axure:Page",w="name",x="Page 2",y="notes",z="style",A="baseStyle",B="627587b6038d43cca051c114ac41ad32",C="pageAlignment",D="near",E="fill",F="fillType",G="solid",H="color",I=0xFFFFFFFF,J="image",K="imageHorizontalAlignment",L="imageVerticalAlignment",M="imageRepeat",N="auto",O="favicon",P="sketchFactor",Q="0",R="colorStyle",S="appliedColor",T="fontName",U="Applied Font",V="borderWidth",W="adaptiveStyles",X="interactionMap",Y="diagram",Z="objects",ba="masters",bb="objectPaths";
+return _creator();
+})());
